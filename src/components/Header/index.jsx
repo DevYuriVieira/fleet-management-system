ee0b21124fc91d94d55bf239175ab94fedc3c66e
ROMULO DA SILVA EVANGELISTA <romulosevangelista@gmail.com>
2026-06-09 19:39:03 -0300

    commit header e footer

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,39 +1,22 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-// Exemplo de uso do Context para pegar alertas (Requisito 6)
-// import { useFrota } from '../../context/FrotaContext'; 
-=======
->>>>>>> Stashed changes
-import { HeaderContainer, Logo, NavMenu, NavLinkStyled, AlertBadge } from './styles';
-import { FaCar, FaBell } from 'react-icons/fa'; // Opcional: npm i react-icons
-
-export default function Header() {
-<<<<<<< Updated upstream
-  // Simulação dos alertas vindo do Context (mude para a lógica real do grupo depois)
-=======
-  
->>>>>>> Stashed changes
-=======
-
+import { NavLink } from 'react-router-dom'; 
 import { HeaderContainer, Logo, NavMenu, NavLinkStyled, AlertBadge } from './styles';
 import { FaCar, FaBell } from 'react-icons/fa'; 
 
 export default function Header() {
-  
->>>>>>> Stashed changes
   const totalAlertas = 3; 
 
   return (
     <HeaderContainer>
+      {}
       <Logo to="/">
         <FaCar size={28} />
         <span>FrotaControl</span>
       </Logo>
 
       <NavMenu>
-        <NavLinkStyled to="/">Home</NavLinkStyled>
+        {}
+        <NavLinkStyled to="/" end>Home</NavLinkStyled>
         <NavLinkStyled to="/manutencoes">Manutenções</NavLinkStyled>
         <NavLinkStyled to="/custos">Custos</NavLinkStyled>
         <NavLinkStyled to="/empresa">Empresa</NavLinkStyled>
