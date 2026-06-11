761cb0cfb32c7eed02b98cea86577e55fa50f5a1
jhonataraibolt <jhonata.raibolt@residente.serratec.org>
2026-06-10 23:29:49 -0300

    refatorando e resolvendo conflitos

--- src/contexts/FrotaContext.jsx
+++ src/contexts/FrotaContext.jsx
@@ -1,5 +1,6 @@
 import React, { createContext, useState, useEffect } from 'react';
-import {getMarcasFipe} from '../services/getDetalhesVeiculo';
+import { frotaLocal } from '../data/Dados';
+import { getDetalhesVeiculoFipe } from '../services/getDetalhesVeiculo.jsx';
 
 export const FrotaContext = createContext();
 
@@ -8,20 +9,62 @@ export const ProvedorFrota = ({ children }) => {
   const [carregando, setCarregando] = useState(true);
   const [error, setError] = useState(null);
 
-  // funcao de busca inicial dos veículos
   useEffect(() => {
-    const buscarVeiculos = async () => {
+    const carregandoVeiculos = async () => {
       try {
         setCarregando(true);
-        const response = await getMarcasFipe('carros');
-        setVeiculos(response);
+
+        const consultaFrota = frotaLocal.map(async (veiculoLocal) => {
+          try {
+            const dadosFipe = await getDetalhesVeiculoFipe(
+              veiculoLocal.tipoVeiculo,
+              veiculoLocal.codigoMarca,
+              veiculoLocal.codigoModelo,
+              veiculoLocal.ano
+            );
+
+            return {
+              //dados locais
+              id: veiculoLocal.id,
+              placa: veiculoLocal.placa,
+              quilometragem: veiculoLocal.quilometragem,
+              descricao: veiculoLocal.descricao,
+              imagens: veiculoLocal.imagens,
+               status: veiculoLocal.status,
+              favoritado: veiculoLocal.favoritado,
+              tipoVeiculo: veiculoLocal.tipoVeiculo,
+              anoFabricacao: veiculoLocal.ano?.split('-')[0],
+              //dados fipe
+              marca: dadosFipe.Marca,
+              modelo: dadosFipe.Modelo,
+              anoModelo: dadosFipe.AnoModelo,
+              combustivel: dadosFipe.Combustivel,
+              valor: dadosFipe.Valor
+            };
+          } catch (erroApi) {
+            console.error(`Erro ao buscar dados FIPE para o veículo ID ${veiculoLocal.id}:`, erroApi);
+            return {
+              ...veiculoLocal,
+              anoFabricacao: veiculoLocal.ano?.split('-')[0],
+              marca: 'Indisponível',
+              modelo: "Indisponível",
+               anoModelo: '---',
+              combustivel: '---',
+              valor: "N/A"
+            };
+          }
+        });
+
+        const frotaCompleta = await Promise.all(consultaFrota);
+        setVeiculos(frotaCompleta);
       } catch (err) {
-        setError('Erro ao carregar os dados da frota.');
+        setError('Erro ao processar e carregar os valores da frota.');
       } finally {
         setCarregando(false);
       }
     };
-    buscarVeiculos();
+
+    carregandoVeiculos();
   }, []);
 
   return (
